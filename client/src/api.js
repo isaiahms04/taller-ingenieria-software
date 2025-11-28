e8de79ee7944cb4780c205f79c28d792d72cf725
isaiahms04 <isaiasignacioms@gmail.com>
2025-11-28 01:26:23 -0300

    cambios al frontend

--- client/src/api.js
+++ client/src/api.js
@@ -1,13 +1,18 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8800',
+  baseURL: process.env.REACT_APP_API_URL, // <-- URL de tu backend en Vercel
 });
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token'); // guardaremos aquí
+  const token = localStorage.getItem('token');
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
+
+export const getEventos = async () => {
+  const response = await api.get('/eventos');
+  return response.data;
+};
 
 export default api;
